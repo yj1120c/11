cdd775fe10e6ed117e278c53171dd7021ce3443c
yj1120c <70882360+yj1120c@users.noreply.github.com>
2020-11-12 15:36:55 +0900

    02

--- main.c
+++ main.c
@@ -4,21 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char *pc;
-	int *pi;
-	double *pd;
+	int i = 10;
+	int *ptr = &i;
 	
-	pc = (char*)10000;
-	pi = (int*)10000;
-	pd = (double*)10000;
-	
-	printf("before : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
-	
-	pc++;
-	pi++;
-	pd++;
-	
-	printf("after : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	printf("i = %d, pi : %p\n", i, ptr);
+	(*ptr)++;
+	printf("i = %d, pi : %p\n", i, ptr);
+	*ptr++;
+	printf("i = %d, pi : %p\n", i, ptr);
 	
 	return 0;
 }
